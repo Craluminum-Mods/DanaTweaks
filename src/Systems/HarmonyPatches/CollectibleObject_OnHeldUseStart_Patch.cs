75baf4db288859a1982b7c1f75c4f648f6f0826e
Craluminum2413 <beware.14b@gmail.com>
2024-04-10 16:44:23 +0300

    don't switch to best tool while holding hammer and looking at anvil

--- src/Systems/HarmonyPatches/CollectibleObject_OnHeldUseStart_Patch.cs
+++ src/Systems/HarmonyPatches/CollectibleObject_OnHeldUseStart_Patch.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.Client.NoObf;
+using Vintagestory.GameContent;
 
 namespace DanaTweaks;
 
@@ -19,6 +20,12 @@ public static class CollectibleObject_OnHeldUseStart_Patch
             return true;
         }
 
+        CollectibleObject heldCollectible = slot?.Itemstack?.Collectible;
+        if (heldCollectible is ItemHammer && blockSel.Block is BlockAnvil)
+        {
+            return true;
+        }
+
         EnumBlockMaterial targetMaterial = blockSel.Block.GetBlockMaterial(byEntity.World.BlockAccessor, blockSel.Position, null);
 
         ItemSlot bestSlot =
@@ -26,9 +33,9 @@ public static class CollectibleObject_OnHeldUseStart_Patch
             .Where(x => x.IsPlayerInventory())
             .SelectMany(x => x)
             .OrderByDescending(x => GetSuperiority(x?.Itemstack?.Collectible?.Tool, targetMaterial))
-            .FirstOrDefault(x => IsFaster(thisObject: x?.Itemstack?.Collectible, otherObject: slot?.Itemstack?.Collectible, targetMaterial), defaultValue: slot);
+            .FirstOrDefault(x => IsFaster(thisObject: x?.Itemstack?.Collectible, otherObject: heldCollectible, targetMaterial), defaultValue: slot);
 
-        if (bestSlot != null && (slot?.Itemstack?.Collectible.Equals(slot?.Itemstack, bestSlot.Itemstack, GlobalConstants.IgnoredStackAttributes)) == false)
+        if (bestSlot != null && heldCollectible.Equals(slot?.Itemstack, bestSlot.Itemstack, GlobalConstants.IgnoredStackAttributes) == false)
         {
             object packetClient = bestSlot.Inventory.TryFlipItems(bestSlot.Inventory.GetSlotId(bestSlot), slot);
             (byEntity.Api as ClientCoreAPI)?.Network.SendPacketClient(packetClient);
